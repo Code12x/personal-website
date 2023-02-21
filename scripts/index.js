711c06400d2dde261bd9ca75de8b07b263db0776
Code12x <87677172+Code12x@users.noreply.github.com>
2023-02-21 11:16:21 -0500

    Finished the projects section

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,19 @@
+let details = document.getElementsByClassName("project-details");
+for(i=0; i<details.length; i++) {
+    details[i].style.display = "none";
+}
 
+function toggleDetails(project) {
+    let details = project.parentNode.getElementsByClassName("project-details")[0];
+    details.style.display == "none" ? showDetails(details) : hideDetails(details)
+}
+
+function showDetails(details) {
+    details.style.display="block";
+    details.parentNode.getElementsByClassName("project-caret")[0].style.transform = "rotate(90deg)";
+}
+
+function hideDetails(details) {
+    details.style.display="none";
+    details.parentNode.getElementsByClassName("project-caret")[0].style.transform = "rotate(0)";
+} 
